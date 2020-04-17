a7302678c9e1354aae6133d90c9ffbd9ba7b73e3
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2020-04-17 18:13:26 +0200

    add helpers functions

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,3 +5,13 @@ export function toCelsius(fahrenheit) {
 export function toFahrenheit(celsius) {
   return (celsius * 9) / 5 + 32;
 }
+
+export function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
